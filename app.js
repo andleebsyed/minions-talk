537d5dda86a9d70c97b17909426f2cee287dd0f5
Syed Andleeb <andleebsyed75@gmail.com>
2021-01-18 22:54:29 +0530

    update : wiring button click - ex03

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 // alert("Hello my name is Syed Andleeb")
-var result = prompt("What's your name")
-alert("Heya " +  result)
+// var result = prompt("What's your name")
+// alert("Heya " +  result)
+var btnTranslate = document.querySelector("#btn-translate")
+btnTranslate.addEventListener("click",function ClickHandler(){ 
+    console.log("I Was Clicked");
+})
+
+
