3382408a281f81b388b517513dbbc00a9ff50fc8
Syed Andleeb <andleebsyed75@gmail.com>
2021-01-19 00:05:28 +0530

    update : output div wired ex-05

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 // var result = prompt("What's your name")
 // alert("Heya " +  result)
 var btnTranslate = document.querySelector("#btn-translate")
+var textInput = document.querySelector("#text-input")
+var textOutput = document.querySelector("#text-output")
+// inputValue.addEventListener("")
 btnTranslate.addEventListener("click",function ClickHandler(){ 
     console.log("I Was Clicked");
+    
+    textOutput.innerText = textInput.value
 })
 
 
