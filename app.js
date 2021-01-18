c39afe3f9a3668e2bfe4cb30a15be5a211111933
Syed Andleeb <andleebsyed75@gmail.com>
2021-01-19 00:49:23 +0530

    update : improvements

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-// alert("Hello my name is Syed Andleeb")
-// var result = prompt("What's your name")
-// alert("Heya " +  result)
 var btnTranslate = document.querySelector("#btn-translate")
 var textInput = document.querySelector("#text-input")
 var textOutput = document.querySelector("#text-output")
 var serverUrl  = "https://api.funtranslations.com/translate/minion.json"
+
 function urlGenerator(input){
- return serverUrl + "?" + "text=" + input
- 
+ return serverUrl + "?" + "text=" + input 
 }
+
 function errorHandler(error){
     alert("There is something wrong with the server!Please try again later")
 }
@@ -20,6 +18,5 @@ function ClickHandler(){
     .then (json => textOutput.innerText = json.contents.translated )
     .catch(errorHandler);
 }
-
 
 btnTranslate.addEventListener("click",ClickHandler)
