30b8cbeb0871ea6fabd54463ec3a8a4645c43fbd
Syed Andleeb <andleebsyed75@gmail.com>
2021-01-19 00:46:39 +0530

    update : added fetch call and error handling

--- app.js
+++ app.js
@@ -4,11 +4,22 @@
 var btnTranslate = document.querySelector("#btn-translate")
 var textInput = document.querySelector("#text-input")
 var textOutput = document.querySelector("#text-output")
-// inputValue.addEventListener("")
-btnTranslate.addEventListener("click",function ClickHandler(){ 
-    console.log("I Was Clicked");
-    
-    textOutput.innerText = textInput.value
-})
+var serverUrl  = "https://api.funtranslations.com/translate/minion.json"
+function urlGenerator(input){
+ return serverUrl + "?" + "text=" + input
+ 
+}
+function errorHandler(error){
+    alert("There is something wrong with the server!Please try again later")
+}
+
+function ClickHandler(){ 
+    var inputText = textInput.value
+    fetch(urlGenerator(inputText))
+    .then(response => response.json())
+    .then (json => textOutput.innerText = json.contents.translated )
+    .catch(errorHandler);
+}
 
 
+btnTranslate.addEventListener("click",ClickHandler)
